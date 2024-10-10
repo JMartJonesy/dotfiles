ecb5cbb5790bc6dcf97b0313907cfa32334d3ad0
Jesse Martinez <sylentstryke@gmail.com>
2024-10-09 18:34:15 -0700

    Update drives.nix

--- drives.nix
+++ drives.nix
@@ -17,7 +17,7 @@
                 format = "vfat";
                 mountpoint = "/boot";
                 mountOptions = [
-                  "defaults"
+                  "umask=0077"
                 ];
               };
             };
@@ -27,13 +27,15 @@
               content = {
                 type = "luks";
                 name = "cryptroot";
-                extraOpenArgs = [
-                  "--allow-discards"
-                  "--perf-no_read_workqueue"
-                  "--perf-no_write_workqueue"
-                ];
-                # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
-                settings = {crypttabExtraOpts = ["fido2-device=auto" "token-timeout=10"];};
+                settings = {
+                  allowDiscards = true;
+                  keyFile = "/tmp/secret.key";
+                  # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
+                  #crypttabExtraOpts = [
+                  #  "fido2-device=auto"
+                  #  "token-timeout=10"
+                  #];
+                };
                 content = {
                   type = "btrfs";
                   extraArgs = ["-L" "nixos" "-f"];
@@ -60,7 +62,7 @@
                     };
                     "/swap" = {
                       mountpoint = "/swap";
-                      swap.swapfile.size = "64G";
+                      swap.swapfile.size = "32G";
                     };
                   };
                 };
